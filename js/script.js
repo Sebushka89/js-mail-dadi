ef24ee9f473f48885d037988379efb6085f34a4c
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-05-31 14:53:04 +0200

    corretta la var i

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var emailUser = prompt('Inserisci la tua Email');
 var emailList =['himaxal626@revutap.com','unnysserrutt-1201@yopmail.com','enunippaja-0231@yopmail.com','elliffegenne-8954@yopmail.com', 'sebastian.rizzo@hotmail.it']
 var check = false;
 
-for (var i=1; i < emailList.length; i++) {
+for (var i = 0; i < emailList.length; i++) {
     if (emailList[i] === emailUser) {
         check = true;
     } 
